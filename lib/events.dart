9a8dbdd9c9fd1a383fb5e34a7054c5f0192ac847
Vladimir Kosinov <kosinovladimir@mail.ru>
2022-11-15 23:04:10 +0300

    commit_class

---
+++ lib/events.dart
@@ -0,0 +1,13 @@
+class Eve {
+  late String name;
+  late String location;
+  late DateTime startDateTime;
+
+  Eve({required this.name, required this.location, required this.startDateTime}) {
+  }
+}
+
+// var e = Event(
+//     name: "Event 1",
+//     location: "Voronezh",
+//     startDateTime: DateTime.now());
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:application_2/custon_icons.dart';
+
+import 'events.dart';
 
 void main() => runApp(FlutterTutorialApp());
 
 class FlutterTutorialApp extends StatelessWidget {
-  const FlutterTutorialApp({Key? key}) : super(key: key);
+  List<Eve> get events => [
+        Eve(name: "eve 1", location: "loc 1", startDateTime: DateTime.now()),
+        Eve(name: "eve 2", location: "loc 2", startDateTime: DateTime.now()),
+        Eve(name: "eve 3", location: "loc 3", startDateTime: DateTime.now()),
+        Eve(name: "eve 4", location: "loc 4", startDateTime: DateTime.now()),
+        Eve(name: "eve 5", location: "loc 5", startDateTime: DateTime.now()),
+        Eve(name: "eve 6", location: "loc 6", startDateTime: DateTime.now()),
+      ];
 
   @override
   Widget build(BuildContext context) {
@@ -16,34 +26,14 @@ class FlutterTutorialApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.brown,
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          // textBaseline: TextBaseline.ideographic,
-          children: [
-            Container(
-              color: Colors.blue,
-              padding: EdgeInsets.all(20.0),
-              child: Text("1"),
-            ),
-            const SizedBox(
-              height: 200,
-            ),
-            Container(
-              color: Colors.red,
-              padding: EdgeInsets.all(30.0),
-              child: Text("1"),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              color: Colors.amber,
-              padding: EdgeInsets.all(40.0),
-              child: Text("1"),
-            )
-          ],
-        ),
+        body: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.all(40),
+            itemCount: events.length,
+            itemBuilder: (_, index) => Text(
+                  "$index -- ${events[index].name} -- ${events[index].location} -- ${events[index].startDateTime}",
+                  style: TextStyle(fontSize: 20),
+                )),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: Text("+"),
